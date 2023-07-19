6da22e458c3cbc9390270af7e5b7973b749c980a
isaacg <iwanyoike65@gmail.com>
2023-07-19 12:46:46 +0300

    MAke error changes on authenticate

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(name: params[:name])
 
-    if user&.authenticate(params[:password])
+    if user && user.authenticate(params[:password])
       render json: { token: encode_token({ user_id: user.id }) }
     else
       render json: { error: 'Invalid username or password' }
     end
   end
 end
+
