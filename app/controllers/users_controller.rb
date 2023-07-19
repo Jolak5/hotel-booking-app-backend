cf12775be5382261b20bc2b0b8da1b31b61b7bbb
isaacg <iwanyoike65@gmail.com>
2023-07-19 12:53:09 +0300

    Change user to @user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,16 +13,17 @@ class UsersController < ApplicationController
     render json: @user
   end
 
-  # POST /users
-  def create
-    @user = User.new(user_params)
+# POST /users
+def create
+  @user = User.new(user_params)
 
-    if user.valid?
-      render json: { token: encode_token({ user_id: user.id }) }
-    else
-      render json: { error: 'Failed to create user' }
-    end
+  if @user.save
+    token = encode_token({ user_id: @user.id })
+    render json: { user: @user, token: token }, status: :created
+  else
+    render json: @user.errors, status: :unprocessable_entity
   end
+end
 
   # PATCH/PUT /users/1
   def update
