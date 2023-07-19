daf3f95baba99637ef6df601bc0d857c8394ca3a
Koshao-sys <92449654+Koshao-sys@users.noreply.github.com>
2023-07-19 12:12:30 +0300

    Updated the user coontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show update destroy]
+  skip_before_action :authorized, only: [:create]
 
   # GET /users
   def index
@@ -17,10 +17,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.save
-      render json: @user, status: :created, location: @user
+    if user.valid?
+      render json: { token: encode_token({ user_id: user.id }) }
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: { error: 'Failed to create user' }
     end
   end
 
@@ -47,6 +47,6 @@ class UsersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :password)
   end
 end
