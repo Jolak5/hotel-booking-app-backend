a72b90797a8c35e537ce54100d6d34472d5be7e8
isaacg <iwanyoike65@gmail.com>
2023-07-23 15:45:27 +0300

    All code done

--- app/serializers/hotel_serializer.rb
+++ app/serializers/hotel_serializer.rb
@@ -1,12 +1,21 @@
 class HotelSerializer < ActiveModel::Serializer
-    attributes :id, :name, :description, :duration, :price, :image
-  
-    def image
-      if object.image.attached?
-        {
-          url: rails_blob_url(object.image)
-        }
-      end
+  include Rails.application.routes.url_helpers
+
+  attributes :id, :name, :description, :duration, :price, :image
+
+  def image
+    Rails.logger.debug "Serializing image for hotel: #{object.id}"
+    if object.image.attached?
+      {
+        url: rails_blob_url(object.image)
+      }
+    else
+      {
+        url: nil
+      }
     end
+  end
+  
+  
   end
   
