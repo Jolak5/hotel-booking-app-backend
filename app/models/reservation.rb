cb9a762ef6988491b56b5ea8a985431e9284aebe
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-07-26 23:53:29 +0200

    Fix linter errors

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,7 +6,7 @@ class Reservation < ApplicationRecord
   validates :hotel_id, presence: true
   validates :reservation_date, presence: true
   validates :duration, numericality: { only_integer: true, greater_than: 0 }
-  
+
   def user_name
     user&.name || 'User information not available'
   end
